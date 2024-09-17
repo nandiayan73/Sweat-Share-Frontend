105d5530e909b762bf980aefcf52dfbc33da570b
Shamsuzzaman <Shamscse.123@gmail.com>
2024-09-17 20:55:00 +0530

    Files Added

---
+++ src/userprofile/components/Highlights.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Circlehighlights from './Circlehighlights';
+const src = "https://fontawesome.com/icons/plus?f=classic&s=light"
+function Highlights() {
+  return (
+    <>
+      <div className="highlights">
+       
+        <Circlehighlights/>
+       
+
+        
+      </div>
+    </>
+  );
+}
+
+export default Highlights
